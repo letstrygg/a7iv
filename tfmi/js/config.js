c49869659b3308ace43fd68afa1cb30aadda4d51
letstrygg <65213506+letstrygg@users.noreply.github.com>
2026-04-02 15:48:51 -0700

    2
    2

---
+++ tfmi/js/config.js
@@ -0,0 +1,3 @@
+// This is your single source of truth for configuration.
+window.SUPABASE_URL = "https://fnwmtytnltmqjaflfwyr.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_y12qZF_dSbUPmV_aieiUgA_CibDsxQV";
