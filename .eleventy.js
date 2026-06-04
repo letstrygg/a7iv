b5537f98f31c9a12be6ff43976375cf3595a4aed
letstrygg <65213506+letstrygg@users.noreply.github.com>
2026-06-04 16:02:02 -0700

    1

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/style.css");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    }
+  };
+};
